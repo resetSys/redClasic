69c34787b8dba6e83bb1c42ce54b034c9eea7676
User大帅 <git config --global user.email you@example.com>
2019-11-28 09:40:20 +0800

    这是依赖的js文件

---
+++ js/components/scroll.js
@@ -0,0 +1,30 @@
+var box = document.getElementById("scroll");
+var ul = box.getElementsByTagName("ul")[0];
+var lis = ul.getElementsByTagName("li");
+var speed = 2;
+
+ul.innerHTML += ul.innerHTML;
+ul.style.width = lis.length * lis[0].offsetWidth + 'px';
+
+function move() {
+    //这里需要对ul.style.left 进行处理 
+    if (ul.offsetLeft < -ul.offsetWidth / 2) {
+        ul.style.left = '0';
+    }
+    if (ul.offsetLeft >= 0) {
+        ul.style.left = -ul.offsetWidth / 2 + 'px';
+    }
+
+    ul.style.left = ul.offsetLeft + speed + 'px';
+}
+
+var timer = setInterval(move, 20);
+
+box.onmouseover = function() {
+    console.log('mouserover');
+    clearInterval(timer);
+}
+
+box.onmouseout = function() {
+    timer = setInterval(move, 20);
+}
